13d280bd64bef3b94452ee0ca5eb7ac0472845ac
matteomiceli <omatteomiceli@gmail.com>
2025-11-02 11:54:59 -0800

    go mod tidy

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
 	onesData := getSheetData("https://docs.google.com/spreadsheets/d/1c2IkaK9iFRRfE5hy8eHzn-YrDt9LSMUN32Jv51Mbt7k/export?format=csv&gid=0")
-	teamsData := getSheetData("https://docs.google.com/spreadsheets/d/1c2IkaK9iFRRfE5hy8eHzn-YrDt9LSMUN32Jv51Mbt7k/export?format=csv&gid=0")
+	teamsData := getSheetData("https://docs.google.com/spreadsheets/d/1c2IkaK9iFRRfE5hy8eHzn-YrDt9LSMUN32Jv51Mbt7k/export?format=csv&gid=924150328")
+	fmt.Println(teamsData)
 }
